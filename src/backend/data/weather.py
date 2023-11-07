ddcb2ff164f9934b34ed5827af0b16e53387cc43
maxjmohr <mamohoc@yahoo.com>
2023-11-07 20:35:08 +0100

    Weather data: automatically detect coordinates of location, build function

--- src/backend/data/weather.py
+++ src/backend/data/weather.py
@@ -2,46 +2,62 @@ import openmeteo_requests
 import requests_cache
 import pandas as pd
 from retry_requests import retry
+from geopy.geocoders import Nominatim
 
-# Setup the Open-Meteo API client with cache and retry on error
-cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
-retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
-openmeteo = openmeteo_requests.Client(session = retry_session)
+def get_weather(location: str):
+	"""
+	Input: location (string)
+	Output: weather data (pandas dataframe)
+	"""
 
-# Make sure all required weather variables are listed here
-# The order of variables in hourly or daily is important to assign them correctly below
-url = "https://api.open-meteo.com/v1/forecast"
-params = {
-	"latitude": 48.1374,
-	"longitude": 11.5755,
-	"daily": ["temperature_2m_max", "temperature_2m_min"],
-	"timezone": "auto",
-	"past_days": 92,
-	"forecast_days": 14,
-	"models": "best_match"
-}
-responses = openmeteo.weather_api(url, params=params)
+	# Get location coordinates
+	geolocator = Nominatim(user_agent="Max Mohr")
+	coor = geolocator.geocode(location)
 
-# Process first location. Add a for-loop for multiple locations or weather models
-response = responses[0]
-print(f"Coordinates {response.Latitude()}°E {response.Longitude()}°N")
-print(f"Elevation {response.Elevation()} m asl")
-print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
-print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+	# Setup the Open-Meteo API client with cache and retry on error
+	cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
+	retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+	openmeteo = openmeteo_requests.Client(session=retry_session)
 
-# Process daily data. The order of variables needs to be the same as requested.
-daily = response.Daily()
-daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
-daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()
+	# Make sure all required weather variables are listed here
+	# The order of variables in hourly or daily is important to assign them correctly below
+	url = "https://api.open-meteo.com/v1/forecast"
+	params = {
+		"latitude": coor.latitude,
+		"longitude": coor.longitude,
+		"daily": ["temperature_2m_max", "temperature_2m_min"],
+		"timezone": "auto",
+		"past_days": 92,
+		"forecast_days": 14,
+		"models": "best_match"
+	}
+	response = openmeteo.weather_api(url, params=params)[0]
 
-daily_data = {"date": pd.date_range(
-	start = pd.to_datetime(daily.Time(), unit = "s"),
-	end = pd.to_datetime(daily.TimeEnd(), unit = "s"),
-	freq = pd.Timedelta(seconds = daily.Interval()),
-	inclusive = "left"
-)}
-daily_data["temperature_2m_max"] = daily_temperature_2m_max
-daily_data["temperature_2m_min"] = daily_temperature_2m_min
+	# Output key information
+	"""print(f"Location: {location}")
+	print(f"Coordinates: {response.Latitude()}°E {response.Longitude()}°N")
+	print(f"Elevation: {response.Elevation()} m asl")
+	print(f"Timezone: {response.Timezone()} {response.TimezoneAbbreviation()}")
+	print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()} s")"""
 
-daily_dataframe = pd.DataFrame(data = daily_data)
-print(daily_dataframe)
+	# Process daily and monthly data
+	daily = response.Daily()
+
+	# Setup daily output
+	daily_data = {"date": pd.date_range(
+		start = pd.to_datetime(daily.Time(), unit = "s"),
+		end = pd.to_datetime(daily.TimeEnd(), unit = "s"),
+		freq = pd.Timedelta(seconds = daily.Interval()),
+		inclusive = "left"
+	)}
+
+	# The order of variables is the same as requested
+	for i,var in enumerate(params["daily"]): 
+		daily_data[var] = daily.Variables(i).ValuesAsNumpy()
+
+	daily_dataframe = pd.DataFrame(data = daily_data)
+	print(daily_dataframe)
+    
+	return daily_dataframe
+
+get_weather("Tuebingen")
