37e86183a2610d9247069c1d44e14782ec701a39
maxjmohr <mamohoc@yahoo.com>
2023-11-07 21:10:35 +0100

    Weather data: add basis for historical data

--- src/backend/data/weather.py
+++ src/backend/data/weather.py
@@ -15,23 +15,34 @@ def get_weather(location: str):
 	coor = geolocator.geocode(location)
 
 	# Setup the Open-Meteo API client with cache and retry on error
-	cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
+	cache_session = requests_cache.CachedSession('.cache', expire_after=-1)
 	retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
 	openmeteo = openmeteo_requests.Client(session=retry_session)
 
 	# Make sure all required weather variables are listed here
-	# The order of variables in hourly or daily is important to assign them correctly below
 	url = "https://api.open-meteo.com/v1/forecast"
 	params = {
 		"latitude": coor.latitude,
 		"longitude": coor.longitude,
 		"daily": ["temperature_2m_max", "temperature_2m_min"],
 		"timezone": "auto",
-		"past_days": 92,
+		"past_days": 5, # as the historical data of the past 5 days is not available via the other url
 		"forecast_days": 14,
 		"models": "best_match"
 	}
 	response = openmeteo.weather_api(url, params=params)[0]
+
+	url_hist = "https://archive-api.open-meteo.com/v1/archive"
+	params = {
+		"latitude": coor.latitude,
+		"longitude": coor.longitude,
+		"start_date": "2010-01-01",
+		"end_date": "2023-11-01",
+		"daily": ["temperature_2m_max", "temperature_2m_min"],
+		"timezone": "auto",
+		"models": "best_match"
+	}
+	response_hist = openmeteo.weather_api(url_hist, params=params)[0]
 
 	# Output key information
 	"""print(f"Location: {location}")
@@ -42,6 +53,7 @@ def get_weather(location: str):
 
 	# Process daily and monthly data
 	daily = response.Daily()
+	print(daily)
 
 	# Setup daily output
 	daily_data = {"date": pd.date_range(
