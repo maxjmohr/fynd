d3c082a57d2de21a09a6a97f1f6ce78056ef3af4
maxjmohr <mamohoc@yahoo.com>
2023-11-12 22:24:28 +0100

    Weather data: add weather codes and their labels, slight adjustments

--- src/backend/data/weather.py
+++ src/backend/data/weather.py
@@ -1,26 +1,41 @@
-import openmeteo_requests
-import requests_cache
-import pandas as pd
-from retry_requests import retry
-from geopy.geocoders import Nominatim
+import os
 from datetime import date, timedelta
+from geopy.geocoders import Nominatim
+import openmeteo_requests
+import pandas as pd
+import requests_cache
+from retry_requests import retry
 
 params = {
 	# Parameters for all APIs
-	"daily": ["temperature_2m_max", "temperature_2m_min", "apparent_temperature_max", "apparent_temperature_min", "sunrise", "sunset",
+	"daily": ["weather_code", "temperature_2m_max", "temperature_2m_min", "apparent_temperature_max", "apparent_temperature_min", "sunrise", "sunset",
 		   "precipitation_sum", "rain_sum", "snowfall_sum", "wind_speed_10m_max"],
 	"timezone": "auto",
 	"models": "best_match",
 	# Parameters for historical API
-	"start_date": "2010-01-01",
-	"end_date": date.today() - timedelta(days = 1), # ecluding yesterday
+	"start_date": "2010-01-02", # Start in the beginning of 2010
+	"end_date": date.today() - timedelta(days = 1), # excluding yesterday
 	# Parameters for current and forecast API
-	"past_days": 0, # as the historical data of the past 5 days is not available via the other url (TBD)
+	"past_days": 0,
 	"forecast_days": 14
 	}
 
-def process_daily(response: openmeteo_requests, params: dict) -> pd.DataFrame:
+def read_weather_codes(path: str) -> pd.DataFrame:
+	""" Reads in the official WMO weather interpretation codes (https://codes.wmo.int/bufr4/codeflag/_0-20-003)
+	Input:  path
+	Output: weather_codes (pandas dataframe)
 	"""
+	# Read the csv file
+	weather_codes = pd.read_csv(path, usecols=["@notation", "rdfs:label"])
+	# Clean the data
+	weather_codes = weather_codes.rename(columns={"@notation": "weather_code", "rdfs:label": "weather_code_label"})
+	weather_codes["weather_code_label"] = weather_codes["weather_code_label"].str.replace("@en", "")
+	weather_codes["weather_code_label"] = weather_codes["weather_code_label"].str[1:-1]
+
+	return weather_codes
+
+def process_daily(response: openmeteo_requests, params: dict) -> pd.DataFrame:
+	""" Processes daily weather data and returns a df
 	Input:  - API response (openmeteo_requests)
 			- Requested dimensions (dict)
 	Output: API output (pandas dataframe)
@@ -42,8 +57,8 @@ def process_daily(response: openmeteo_requests, params: dict) -> pd.DataFrame:
 
 	return pd.DataFrame(data = daily_data)
 
-def get_weather(location: str, params: dict) -> pd.DataFrame:
-	"""
+def main(location: str, params: dict) -> pd.DataFrame:
+	""" Unites historical, current and future weather data
 	Input: location (string)
 	Output: weather data (pandas dataframe)
 	"""
@@ -88,6 +103,14 @@ def get_weather(location: str, params: dict) -> pd.DataFrame:
 	# Process daily data
 	daily_df = pd.concat([daily_df, process_daily(response, params)], ignore_index=True)
 
+	###--- Add weather code labels ---###
+	# Get the directory of the current script
+	current_script_directory = os.path.dirname(os.path.abspath(__file__))
+
+	# Merge the weather codes to the dataframe
+	weather_codes = read_weather_codes(os.path.join(current_script_directory, "../../../res/master_data/weather_codes.csv"))
+	daily_df = daily_df.merge(weather_codes, on="weather_code", how="left")
+
 	# Output key information
 	"""print(f"Location: {location}")
 	print(f"Coordinates: {response.Latitude()}°E {response.Longitude()}°N")
@@ -98,4 +121,5 @@ def get_weather(location: str, params: dict) -> pd.DataFrame:
    
 	return daily_df
 
-get_weather("Tuebingen", params)
+if __name__ == "__main__":
+    main("Tuebingen", params)
