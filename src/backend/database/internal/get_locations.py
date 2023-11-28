57c36d4318b2a99029d8e99a89deecac8a307b0b
maxjmohr <mamohoc@yahoo.com>
2023-11-28 19:45:23 +0100

    Database_internal: add database connection to locations, other optimizations

--- src/backend/database/internal/get_locations.py
+++ src/backend/database/internal/get_locations.py
@@ -1,3 +1,11 @@
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+import db_helpers
+
 import geojson
 from geopy import distance
 from geopy.geocoders import Nominatim
@@ -20,7 +28,7 @@ from shapely.geometry import Point, mapping
 ###------| Step 2: Get any relevant geographical data of target locations |------###
 
 class GeographicalData:
-    def __init__(self, city: str, country: str = None) -> [pd.DataFrame, list, dict]:
+    def __init__(self, city: str, country: str = None) -> None:
         if country is not None:
             self.target = city + ", " + country
         else:
@@ -28,12 +36,11 @@ class GeographicalData:
         name = ["Felix Koehn", "Max Mohr", "Lukas Schick", "Jakob Zgonc"]
         self.geolocator = Nominatim(user_agent=choice(name))
 
-    def get_info_and_coordinates(self, shape: str = "polygon"):
+    def get_info_and_coordinates(self, shape: str = "polygon") -> [pd.DataFrame, dict]:
         ''' Get general information and geographical data on the target location
         Input:  - self.target
                 - shape: str, either "polygon" or "circle"
         Output: - dataframe with geographical data
-                - bounding_box: bounding box of the city. Only returned if shape is "polygon"
                 - geojson: geojson of the city
         '''
         # API call
@@ -63,18 +70,21 @@ class GeographicalData:
             country = None
 
         # Get coordinates and other geographical data
+        location_id = results["place_id"]
         adress_type = results["addresstype"]
         lat = results["lat"]
         lon = results["lon"]
 
-        # Get bounding box and rearrage it (bottom left (lon&lat) and top right (lon&lat))
-        bounding_box = [results["boundingbox"][2], results["boundingbox"][0], results["boundingbox"][3], results["boundingbox"][1]]
+        # Get bounding box and rearrage it (bottom left (lat&lon) and top right (lat&lon))
+        bounding_box = [results["boundingbox"][0], results["boundingbox"][2], results["boundingbox"][1], results["boundingbox"][3]]
 
         # Calculate radius from center to bottom left corner of bounding box in km (always round up)
-        radius_km = ceil(distance.great_circle([lat,lon], [bounding_box[1],bounding_box[0]]).km)
+        radius_km = ceil(distance.great_circle([lat,lon], [bounding_box[0],bounding_box[1]]).km)
 
         # Create dataframe
-        df = pd.DataFrame(data = {"city": [city], "district": [district], "state": [state], "country": [country], "adress_type": [adress_type], "lat": [lat], "lon": [lon], "radius_km": [radius_km]})
+        data = pd.DataFrame(data = {"location_id": [location_id], "city": [city], "district": [district], "state": [state], "country": [country], "adress_type": [adress_type],
+                                  "lat": [lat], "lon": [lon], "radius_km": [radius_km],
+                                  "box_bottom_left_lat": [bounding_box[0]], "box_bottom_left_lon": [bounding_box[1]], "box_top_right_lat": [bounding_box[2]], "box_top_right_lon": [bounding_box[3]]})
 
         # Get geojson
         if shape == "polygon":
@@ -89,13 +99,18 @@ class GeographicalData:
         else:
             raise ValueError("Invalid shape. Shape can either be 'polygon' or 'circle'.")
     
-        return df, bounding_box, geo_json
+        return data, geo_json
     
-df, bounding_box, geo_json = GeographicalData("Tuebingen", "Germany").get_info_and_coordinates(shape="polygon")
+data, geo_json = GeographicalData("Tuebingen", "Germany").get_info_and_coordinates(shape="polygon")
 
-print(df)
-#print(bounding_box)
+print(data)
 #print(geo_json)
 
 
 ###------| Step 3: Store data in database |------###
+
+conn, cur, engine = db_helpers.connect_to_db()
+db_helpers.create_db_object(conn, cur, object="core_locations")
+db_helpers.insert_data(engine, data, table="core_locations")
+print(db_helpers.fetch_data(engine, total_object="core_locations"))
+db_helpers.disconnect_from_db(conn, cur, engine)
