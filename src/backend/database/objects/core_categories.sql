7c25ebbafdc371349fa6e01ea2d84144c597667f
maxjmohr <mamohoc@yahoo.com>
2024-01-18 16:04:45 +0100

    Add categories and dimensions, add some descriptions

--- src/backend/database/objects/core_categories.sql
+++ src/backend/database/objects/core_categories.sql
@@ -1,7 +1,7 @@
 CREATE TABLE core_categories (
     category_id     INTEGER NOT NULL,
     category        VARCHAR(255) NOT NULL,
-    description     VARCHAR(255),
+    description     VARCHAR(500),
     updated_at      TIMESTAMP WITHOUT TIME ZONE DEFAULT (now() AT TIME ZONE 'utc-01'),
     PRIMARY KEY (category_id)
 );
@@ -16,10 +16,11 @@ COMMENT ON COLUMN core_categories.updated_at IS 'Timestamp of the last update.';
 
 -- Create the master data
 INSERT INTO core_categories (category_id, category, description, updated_at)
-VALUES (1, 'safety', 'Safety', now()),
-       (2, 'weather', 'Weather', now()),
-       (3, 'culture', 'Culture', now()),
-       (4, 'cost', 'Cost dimension includes travel costs, accommodation costs and cost of living.', now()),
-       (5, 'geography', 'Geography', now()),
-       (6, 'health', 'Health', now())
+VALUES (1, 'safety', 'Safety encompasses measures and conditions that contribute to the protection of individuals, property, and well-being. It includes aspects such as crime rates, personal freedom, political stability and ecological threats', now()),
+       (2, 'weather', 'Weather focuses on atmospheric conditions that influence the environment. It encompasses temperature, precipitation, humidity, and other meteorological factors that can impact daily life and activities.', now()),
+       (3, 'culture', 'Culture refers to the offered ', now()),
+       (4, 'cost', 'Costs involve the financial aspects associated with traveling to and staying in a particular location. It includes travelling and accommodation costs as well as general costs of living.', now()),
+       (5, 'geography', 'Geography encompasses the physical features and characteristics of a location. It includes landforms, overall terrain and protruding landmarks that define the physical environment.', now()),
+       (6, 'reachability', 'Reachability focuses on the ease of access to and from a location. It includes transportation infrastructure, connectivity, and the availability of various modes of transportation.', now()),
+       (7, 'health', 'Health involves the well-being of individuals and the quality of healthcare services within a region. It encompasses factors such as suggested vaccinations, disease prevalence, and overall public health measures.', now())
 ;
