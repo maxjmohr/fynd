0ef995dd542e13b8a0b74aea929287623496090f
Jakob Zgonc <93334939+jzgonc@users.noreply.github.com>
2024-01-19 18:49:56 +0100

    Not use, hence deleted

--- src/frontend/destination_search/destinations/filters.py
+++
@@ -1,7 +0,0 @@
-import django_filters
-from .models import CoreLocations
-
-class LocationsFilterset(django_filters.FilterSet):
-    class Meta:
-        model = CoreLocations
-        fields = ['country']
