c30bb0e62a70b61984ad4fb4f456ad9e0d090ed3
Jakob Zgonc <93334939+jzgonc@users.noreply.github.com>
2024-02-04 00:34:03 +0100

    Split settings into base (dev) and prod

--- src/frontend/destination_search/destination_search/settings/__init__.py
+++ src/frontend/destination_search/destination_search/settings/__init__.py
@@ -18,7 +18,7 @@ env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
