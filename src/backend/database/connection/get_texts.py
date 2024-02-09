3414221d5e2259b68e1fa28cf3ed430f038c6abc
maxjmohr <mamohoc@yahoo.com>
2024-02-09 13:51:32 +0100

    Text gen pipeline adapt prompts, prepare data, include database

--- src/backend/database/connection/get_texts.py
+++ src/backend/database/connection/get_texts.py
@@ -5,6 +5,7 @@ parent_dir = os.path.dirname(os.path.realpath(__file__+"/../../"))
 sys.path.append(parent_dir)
 
 from database.db_helpers import Database
+from datetime import datetime
 from openai import OpenAI
 import pandas as pd
 
@@ -12,12 +13,14 @@ import pandas as pd
 class PromptEngine:
     "Class to generate prompts for the GPT chatbot and return the generated texts"
 
-    def __init__(self, model:str="gpt-3.5-turbo", temperature:float=0.) -> None:
+    def __init__(self, db:Database, model:str="gpt-3.5-turbo", temperature:float=0.) -> None:
         ''' Initialize the class
-        Input:  - model: the model to be used for the chatbot
+        Input:  - db: the database connection
+                - model: the model to be used for the chatbot
                 - temperature: the temperature to be used for the chatbot
         Output: None
         '''
+        self.db = db
         self.model = model
         self.temperature = temperature
         self.client = OpenAI()
@@ -30,7 +33,13 @@ class PromptEngine:
         "Create the message for the general text of a location category"
         return {
             "role": "system",
-            "content": "You are a data interpretation system, adept at analyzing numerical variables and generating descriptive insights."
+            "content": """
+You are a data interpretation system, adept at analyzing numerical variables and generating descriptive insights of travel destinations.\n
+You are asked to generate a general text for a destination category based on the distances to the median of the dimensions.\n
+A positive distance indicates that the destination is above the median, while a negative distance indicates that the destination is below the median.\n
+Larger distances indicate a larger deviation from the median.\n
+Never generate a text with numeric values such as distances but rather use the distances to generate a qualitative description of the destination.
+"""
         }
 
 
@@ -39,25 +48,31 @@ class PromptEngine:
         "Create the message for the anomaly text of a location category"
         return {
             "role": "system",
-            "content": "You are a data interpretation system, adept at analyzing numerical variables and generating descriptive insights based on detected anomalies."
+            "content": """
+You are a data interpretation system, adept at analyzing numerical variables and generating descriptive insights of travel destinations based on detected anomalies.\n
+You are asked to generate a general text for a destination category based on the distances to the anomaly bounds of the dimensions.\n
+A positive distance indicates that the destination is above the upper bound, while a negative distance indicates that the destination is below the lower bound.\n
+Larger distances indicate a larger deviation from the bounds (i.e. a stronger anomaly).\n
+Never generate a text with numeric values such as distances but rather use the distances to generate a qualitative description of the destination.
+"""
         }
 
 
     @staticmethod
-    def content_seasonal_distances(distances:pd.DataFrame, which_distance:str) -> str:
+    def content_seasonal_distances(data:pd.DataFrame, which_distance:str) -> str:
         ''' Get the content part of the seasonal distances
-        Input:  - distances: the distances along with category and dimension id
+        Input:  - data: the data along with category and dimension id
                 - which_distance: the distance to be used for the content
         Output: formatted_content: the formatted content for the seasonal distances
         '''
         # Assert that there is only one unique category_id
-        assert distances["category_id"].nunique() == 1, "The category_id is not unique."
+        assert data["category_id"].nunique() == 1, "The category_id is not unique."
 
         # Get distance metric string
         distance_metric = which_distance.replace("distance_to_", "")
 
-        # Order distances by start_date and dimension_id
-        distances = distances.sort_values(by=["start_date", "dimension_id"], ascending=True)
+        # Order data by start_date and dimension_id
+        data = data.sort_values(by=["start_date", "dimension_id"], ascending=True)
 
         # Initialize an empty string to store the formatted content
         formatted_content = f"""
@@ -66,7 +81,7 @@ The following table shows the distances to the {distance_metric}s for the corres
 """
 
         # Iterate through each row of the DataFrame
-        for _, row in distances.iterrows():
+        for _, row in data.iterrows():
             # Format the time period
             time_period = f"* Time period: {row['start_date']} - {row['end_date']}\n"
             formatted_content += time_period
@@ -83,17 +98,20 @@ The following table shows the distances to the {distance_metric}s for the corres
 
 
     @staticmethod
-    def content_non_seasonal_distances(distances:pd.DataFrame, which_distance:str) -> str:
+    def content_non_seasonal_distances(data:pd.DataFrame, which_distance:str) -> str:
         ''' Get the content part of the non-seasonal distances
-        Input:  - distances: the distances along with category and dimension id
+        Input:  - data: the data along with category and dimension id
                 - which_distance: the distance to be used for the content
         Output: formatted_content: the formatted content for the non-seasonal distances
         '''
+        # Assert that there is only one unique category_id
+        assert data["category_id"].nunique() == 1, "The category_id is not unique."
+
         # Get distance metric string
         distance_metric = which_distance.replace("distance_to_", "")
 
-        # Order distances by dimension_id
-        distances = distances.sort_values(by="dimension_id", ascending=True)
+        # Order data by dimension_id
+        data = data.sort_values(by="dimension_id", ascending=True)
 
         # Initialize an empty string to store the formatted content
         formatted_content = f"""
@@ -103,7 +121,7 @@ The following table shows the distances to the {distance_metric}s for the corres
         
         # Format the dimension name and distance
         dimension_distance_pairs = ""
-        for dimension_name, distance in zip(distances['dimension_name'], distances[which_distance]):
+        for dimension_name, distance in zip(data['dimension_name'], data[which_distance]):
             dimension_distance_pairs += f"* {dimension_name}: {distance}\n"
 
         # Add the dimension name and distance to the formatted content
@@ -112,53 +130,53 @@ The following table shows the distances to the {distance_metric}s for the corres
         return formatted_content
 
 
-    def create_message_general_user(self, distances:pd.DataFrame) -> dict:
+    def create_message_general_user(self, data:pd.DataFrame) -> dict:
         ''' Create the message for the user
-        Input:  distances: the distances along with category and dimension id
+        Input:  data: the data along with category and dimension id
         Output: message: message to be sent to the chatbot
         '''
         # Assert that there is only one unique category_id
-        assert distances["category_id"].nunique() == 1, "The category_id is not unique."
+        assert data["category_id"].nunique() == 1, "The category_id is not unique."
 
         message = {"role": "user"}
 
         # For weather, reachability and travel/accommodation costs
-        if distances["category_id"].any() in [2, 6] or distances["dimension_id"].any() in [41, 42]:
+        if data["category_id"].any() in [2, 6] or data["dimension_id"].any() in [41, 42]:
 
             message["content"] = f"""
 Use a maximum of five sentences in the paragraph to generally describe the characteristics of the location for the following dimensions of this category:
-{self.content_seasonal_distances(distances, "distance_to_median")}
+{self.content_seasonal_distances(data, "distance_to_median")}
 """
 
         else:
 
             message["content"] = f"""
 Use a maximum of five sentences in the paragraph to generally describe the characteristics of the location for the following dimensions of this category:
-{self.content_non_seasonal_distances(distances, "distance_to_median")}
+{self.content_non_seasonal_distances(data, "distance_to_median")}
 """
         return message
 
 
-    def create_message_anomaly_user(self, distances:pd.DataFrame, text_general:str) -> dict:
+    def create_message_anomaly_user(self, data:pd.DataFrame, text_general:str) -> dict:
         ''' Create the message for the user
-        Input:  - distances: the distances along with category and dimension id
+        Input:  - data: the data along with category and dimension id
                 - text_general: generated general text for context
         Output: message: message to be sent to the chatbot
         '''
 
         # Assert that there is only one unique category_id
-        assert distances["category_id"].nunique() == 1, "The category_id is not unique."
+        assert data["category_id"].nunique() == 1, "The category_id is not unique."
 
         message = {"role": "user"}
 
         # For weather, reachability and travel/accommodation costs
-        if distances["category_id"].any() in [2, 6] or distances["dimension_id"].any() in [41, 42]:
+        if data["category_id"].any() in [2, 6] or data["dimension_id"].any() in [41, 42]:
 
             message["content"] = f"""
-Use a maximum of five sentences in the paragraph to describe the anomalies of the location for the dimensions of the category '{distances['category_name']}'.\n
-{self.content_seasonal_distances(distances, "distance_to_bound")}
+Use a maximum of five sentences in the paragraph to describe the anomalies of the travel destination for the dimensions of the category '{data['category_name']}'.\n
+{self.content_seasonal_distances(data, "distance_to_bound")}
 Generate the anomaly text in a way that it is consistent with the general text and fits right after the general text.\n
-Text: '''\n
+General text: '''\n
 {text_general}\n
 '''
 """
@@ -166,10 +184,10 @@ Text: '''\n
         else:
 
             message["content"] = f"""
-Use a maximum of five sentences in the paragraph to describe the anomalies of the location for the dimensions of the category '{distances['category_name']}'.\n
-{self.content_non_seasonal_distances(distances, "distance_to_bound")}
+Use a maximum of five sentences in the paragraph to describe the anomalies of the travel destination for the dimensions of the category '{data['category_name']}'.\n
+{self.content_non_seasonal_distances(data, "distance_to_bound")}
 Generate the anomaly text in a way that it is consistent with the general text and fits right after the general text.\n
-Text: '''\n
+General text: '''\n
 {text_general}\n
 '''
 """
@@ -183,28 +201,109 @@ Text: '''\n
         '''
         response = self.client.chat.completions.create(
             model=self.model,
-            messages=messages
+            messages=messages,
+            temperature=self.temperature
         )
         return response.choices[0].message
 
 
-    def prompt(self, distances:pd.DataFrame) -> list:
+    def prompt(self, data:pd.DataFrame) -> pd.DataFrame:
         ''' Create prompts from the messages
-        Input:  distances: the distances along with category and dimension id
-        Output: prompt: prompt as messages to be sent to the chatbot
+        Input:  data: the data along with category and dimension id
+        Output: df: the resulting dataframe with the general and anomaly texts
         '''
+        # Initiate resulting dataframe
+        df = pd.DataFrame({
+            "location_id": [data["location_id"].iloc[0]],
+            "category_id": [data["category_id"].iloc[0]],
+            "ref_start_location_id": [data["ref_start_location_id"].iloc[0]],
+        })
+
         # Get general text
+        print(f"{datetime.now()} - Generating the general text for '{data['location_city'].iloc[0]} ({data['location_country'].iloc[0]})', category '{data['category_id'].iloc[0]}', start location '{data['start_location_city'].iloc[0]} ({data['start_location_country'].iloc[0]})'.")
         messages = [
             self.message_general_system,
-            self.create_message_general_user(distances)
+            self.create_message_general_user(data)
         ]
-        text_general = self.get_response(messages)
+        df["text_general"] = self.get_response(messages)
 
-        # Get anomaly text
-        messages = [
-            self.message_anomaly_system,
-            self.create_message_anomaly_user(distances, text_general)
-        ]
-        text_anomaly = self.get_response(messages)
+        # Reachability extra sentence
+        if data["category_id"].iloc[0] == 6:
+            assert data["ref_start_location_id"] != -1, "There is no reference start location related to the reachability scores."
 
-        return [text_general, text_anomaly]
+            df["text_general"] = f"""To calculate the reachability, we use a fixed selection of reference start locations.
+Based on your selected start location, the reference start location is {data['start_location_city'].iloc[0]} ({data['start_location_country'].iloc[0]}).\n
+{df['text_general'].iloc[0]}
+"""
+
+        # Get anomaly text (if there are anomalies detected)
+        if data["distance_to_bound"].isnull().all():
+            print(f"{datetime.now()} - No anomalies for '{data['location_name'].iloc[0]}v, category '{data['category_id'].iloc[0]}', start location '{data['start_location_name'].iloc[0]}'.")
+            df["text_anomaly"] = None
+
+        else:
+            print(f"{datetime.now()} - Generating the anomaly text for '{data['location_name'].iloc[0]}', category '{data['category_id'].iloc[0]}', start location '{data['start_location_name'].iloc[0]}'.")
+            messages = [
+                self.message_anomaly_system,
+                self.create_message_anomaly_user(data[data["distance_to_bound"].notnull()], df["text_general"])
+            ]
+            df["text_anomaly"]  = self.get_response(messages)
+
+        # Add start_date and end_date
+        df["start_date"] = datetime(2023, 1, 1).date()
+        df["end_date"] = datetime(2099, 12, 31).date()
+
+        # Restructure data and insert into db
+        df = df[["location_id", "category_id", "start_date", "end_date", "ref_start_location_id", "text_general", "text_anomaly"]]
+        self.db.insert_data(texts, "core_texts")
+
+        return df
+
+
+def prepare_text_generation(db:Database) -> pd.DataFrame:
+    ''' Preprocess the data from core_scores into the batches for the text generation
+    Input:  db: the database connection
+    Output: data: the preprocessed data
+    '''
+    # Load data
+    scores = db.fetch_data("core_scores")
+    loaded_texts = db.fetch_data("core_texts")
+    locations = db.fetch_data("core_locations")
+    start_locations = db.fetch("core_ref_start_locations")
+
+    scores = scores.groupby(["location_id", "category_id", "ref_start_location_id"])
+
+    # Check whether texts for groups were already generated
+    scores = scores.filter(lambda x: x.name not in loaded_texts[["location_id", "category_id", "ref_start_location_id"]])
+    scores = scores.reset_index(drop=True)
+
+    # Join the names of the locations and start_locations
+    scores = scores.merge(
+        locations[["location_id", "city", "country"]],
+        on="location_id", how="inner"
+        )
+    scores.rename(columns={"city": "location_city", "country": "location_country"}, inplace=True)
+
+    scores = scores.merge(
+        start_locations[["ref_start_location_id", "city", "country"]],
+        left_on="location_id", right_on="ref_start_location_id", how="left"
+        )
+    scores.rename(columns={"city": "start_location_city", "country": "start_location_country"}, inplace=True)
+
+    return scores
+
+
+if __name__ == "__main__":
+    # Connect to database
+    db = Database()
+    db.connect()
+
+    # Get data
+    data = prepare_text_generation(db)
+
+    # Generate texts
+    prompt_engine = PromptEngine(db)
+    texts = data.apply(prompt_engine.prompt, axis=1)
+
+    # Disconnect from database
+    db.disconnect()
